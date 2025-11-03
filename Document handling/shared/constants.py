ee0b6ea104407ec26da2fbae90e9b8f1641a3b60
Vlad <vladislavr873@gmail.com>
2025-11-03 18:06:33 +0300

    Add files via upload

---
+++ Document handling/shared/constants.py
@@ -0,0 +1,10 @@
+from shared.load_library import np
+
+DEBUG_INFO = False
+DEBUG_IMAGE = False
+
+FORMAT_IMAGE_FILE = "*.jpg *.jpeg *.png *.bmp"
+OUTPUT_PATH = "results/"
+
+MAX_DIMENSION = 1000
+KERNEL = np.ones((5,5), np.uint8)
---
+++ Document handling/shared/load_library.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+import numpy as np
+import sys
+import os
+
+
+from tkinter import filedialog
+
+from datetime import datetime, date
